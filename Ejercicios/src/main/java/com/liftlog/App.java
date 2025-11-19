f1ab9c67370575405e24a0a0c9c8ac3c6b000887
gabduoc <gab.zepeda@duocuc.cl>
2025-11-19 19:25:13 -0300

    First Commit And Push

---
+++ Ejercicios/src/main/java/com/liftlog/App.java
@@ -0,0 +1,13 @@
+package com.liftlog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
